c87fc693c9ff8a28fd778d536001ae07b774c517
Sandeep Kumar <sandy.2087@gmail.com>
2023-05-11 17:23:57 +0530

    added new files

--- ec2-from-remote-state/main.tf
+++ ec2-from-remote-state/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "terraform-cloud-vm" {
   ami                    = var.ami
   instance_type          = var.instance_type
   count                  = 1
-  key_name               = "terraform-cloud-key"
+  key_name               = "terraform"
   user_data              = file("install.sh")
   subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]          
   vpc_security_group_ids = [aws_security_group.sg.id,]
